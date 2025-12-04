ff900ed3d40d16cbf359808ecf362379d708589a
Phantasmysteria <wallacechan58185@gmail.com>
2025-12-04 12:01:13 +0800

    initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# AoC-2015
+
+It occured to me that even though I knew that 2025 was the 10th anniversary of Advent of Code, I have not actually done the older AoC events. So here I am, visiting the very first event.
+
+<sub><sup>the beginning begins</sup></sub>
---
+++ aocutils.py
@@ -0,0 +1,20 @@
+from typing import List, Any
+import itertools
+import pprint
+
+# Quick way to convert input file into list of lines
+def listLines(day: str, part: str) -> List[str]:
+    with open(f"inputD{day}Ex.txt" if part == "ex" else f"inputD{day}P{part}.txt", "r") as f:
+        return [item.rstrip() for item in f.readlines()]
+
+# Quick way to convert input file into list of list of lines separated by delimiter
+def listLinesWithGroup(day: str, part: str, groupSep: str) -> List[str]:
+    with open(f"inputD{day}Ex.txt" if part == "ex" else f"inputD{day}P{part}.txt", "r") as f:
+        return [list(group) for key, group in itertools.groupby([item.rstrip() for item in f.readlines()], lambda x: x == groupSep) if not key]
+    
+def outputToTextFile(data: Any) -> None:
+    with open("output.txt", "w") as f:
+        f.write(pprint.pformat(data))
+
+if __name__ == '__main__':
+    print("Why are you running this file?")
