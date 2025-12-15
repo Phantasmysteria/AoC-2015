bd2905db30f8f33d38c2671ac454a691c8d3fd19
Phantasmysteria <wallacechan58185@gmail.com>
2025-12-15 14:37:27 +0800

    feat: add day 09

---
+++ Day09/D09P2.py
@@ -0,0 +1,41 @@
+import sys
+sys.path.append('..')
+import aocutils
+import re
+from collections import deque
+
+data = {}
+data["00-raw"] = aocutils.listLines(day="09", part="1")
+dataLength = len(data["00-raw"])
+
+data["01"] = [re.search("(\\w*) to (\\w*) = (\\d*)", line) for line in data["00-raw"]]
+data["02-adjList"] = {}
+
+for pair in data["01"]:
+    src, dest, weight = pair.group(1), pair.group(2), int(pair.group(3))
+    if src not in data["02-adjList"]:
+        data["02-adjList"][src] = []
+    if dest not in data["02-adjList"]:
+        data["02-adjList"][dest] = []
+    data["02-adjList"][src].append((dest, weight))
+    data["02-adjList"][dest].append((src, weight))
+
+data["03-completePaths"] = []
+
+queue = deque((0, (loc,)) for loc in data["02-adjList"].keys())
+
+while len(queue):
+    cost, locPath = queue.popleft()
+
+    if len(locPath) == len(data["02-adjList"].keys()):
+        data["03-completePaths"].append((cost, locPath))
+        continue
+    
+    queue.extend([(cost + nCost, (*locPath, neighbour)) for neighbour, nCost in data["02-adjList"][locPath[-1]] if neighbour not in locPath])    
+
+data["04"] = sorted(data["03-completePaths"], key=lambda x: x[0])
+
+output = data["04"][-1][0]
+print(output)
+
+aocutils.outputToTextFile(data)
